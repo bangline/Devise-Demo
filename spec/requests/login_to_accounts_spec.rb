8c0808b2e898690ad1d27d07fc1b688312a5a8ba
Dave Kennedy <david@bangline.co.uk>
2011-12-05 00:56:27 +0000

    Add another test, just to be sure

--- spec/requests/login_to_accounts_spec.rb
+++ spec/requests/login_to_accounts_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe "LoginToAccounts" do
+  before do
+    other_account = Factory.create(:account)
+    @invalid_user = Factory.create(:user, account: other_account)
+    @account = Factory.create(:account, subdomain: "test-account")
+    @user = Factory.create(:user, account: @account)
+    Capybara.app_host = "http://test-account.example.com"
+    visit '/'
+  end
+
   describe "log in to a valid account" do
     before do
-      @account = Factory.create(:account, subdomain: "test-account")
-      @user = Factory.create(:user, account: @account)
-      Capybara.app_host = "http://test-account.example.com"
-      visit '/'
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
       click_button 'Sign in'
@@ -14,6 +19,18 @@ describe "LoginToAccounts" do
 
     it "will notify me that I have logged in successfully" do
       page.should have_content "Signed in successfully"
+    end
+  end
+
+  describe "fail login for valid user wrong account" do
+    before do
+      fill_in 'Email', with: @invalid_user.email
+      fill_in 'Password', with: @invalid_user.password
+      click_button 'Sign in'
+    end
+
+    it "will notify me that I have logged in successfully" do
+      page.should_not have_content "Signed in successfully"
     end
   end
 end
